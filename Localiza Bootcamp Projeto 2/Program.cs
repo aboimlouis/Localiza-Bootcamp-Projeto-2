f9aa10081524d49273b81761c72c917a4a331b47
Louis Aboim <aboimlouis@gmail.com>
2021-03-05 17:05:13 -0300

    Base para o segundo projeto do bootcamp da localiza

---
+++ Localiza Bootcamp Projeto 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Localiza_Bootcamp_Projeto_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
